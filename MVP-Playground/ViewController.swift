422e2763e08785b8c2c041e3f7efc9f863d82445
Bennet van der Linden <vanderlinden.bennet@gmail.com>
2020-01-24 11:15:33 +0100

    Initial Commit

---
+++ MVP-Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP-Playground
+//
+//  Created by Bennet van der Linden on 24/01/2020.
+//  Copyright © 2020 Bennet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
